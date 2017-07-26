c463d51fbc69b73775aba1a6db5be35155061fd5
Glenn Goodrich <glenn.goodrich@gmail.com>
2017-07-26 13:20:25 -0400

    oops

--- cmds/client/redux/src/actions/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.incrementCounter = function (delta) { return ({
-    type: 'INCREMENT_COUNTER',
-    delta: delta
-}); };
-exports.resetCounter = function () { return ({
-    type: 'RESET_COUNTER'
-}); };
